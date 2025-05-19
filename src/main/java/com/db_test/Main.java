84266d7914e4d6a0c1390e2fe4b4716cef11ba66
AungPaingK <saiyankhant.mdy828@gmail.com>
2025-05-19 16:09:04 +0630

    MySql dependency in pom.xml

--- src/main/java/com/db_test/Main.java
+++ src/main/java/com/db_test/Main.java
@@ -1,12 +1,44 @@
 package com.db_test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
+    private String ip = "localhost";
+    private String port = "3306";
+    private String dbname = "test_db";
+    private String username = "root";
+    private String password = "";
+
+    protected Connection get_DB_Connection() {
+        Connection con = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://" + ip + ":" + port + "/" + dbname,
+                    username, password);
+            System.out.println("Successful Connection.");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return con;
+    }
+
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Main m = new Main();
+        Connection con = m.get_DB_Connection();
+
+        try {
+            if (con != null) {
+                con.close();
+                System.out.println("Connection is closed.");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
